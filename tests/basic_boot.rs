4065ae93c5fb7ec07531c693c6b385f14b39130b
John Scott <johnrscott0@gmail.com>
2023-06-14 20:37:42 +0100

    Added up to integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,31 +1,24 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(rustos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(rustos::test_runner)]
 
 use core::panic::PanicInfo;
 use rustos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rustos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
