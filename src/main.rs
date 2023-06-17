884c5df86c1089f3669575480d92c545b26467b7
John Scott <johnrscott0@gmail.com>
2023-06-17 16:23:29 +0100

    With page table functions/page frame allocation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use rustos::println;
+use rustos::{println, memory::{translate_addr, self, BootInfoFrameAllocator}};
+use x86_64::structures::paging::{Translate, Page};
 
 entry_point!(kernel_main);
 
@@ -18,30 +19,23 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     rustos::init();
 
+    ////////////////////////
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };;
 
-    for (i, entry) in l4_table.iter().enumerate() {
-	use x86_64::structures::paging::PageTable;
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-	if !entry.is_unused() {
-	    println!("L4 Entry {}: {:?}", i, entry);
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
-	    // get the physical address from the entry and convert it
-	    let phys = entry.frame().unwrap().start_address();
-	    let virt = phys.as_u64() + boot_info.physical_memory_offset;
-	    let ptr = VirtAddr::new(virt).as_mut_ptr();
-	    let l3_table: &PageTable = unsafe { &*ptr };
-
-	    // print non-empty entries of the level 3 table
-	    for (i, entry) in l3_table.iter().enumerate() {
-		if !entry.is_unused() {
-		    println!("  L3 Entry {}: {:?}", i, entry);
-		}
-	    }
-	}
-    }
-
+    //////////////////////
     
     #[cfg(test)]
     test_main();
