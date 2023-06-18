d99db59d212854e7316cc638825cb96361dd7648
John Scott <johnrscott0@gmail.com>
2023-06-18 20:55:46 +0100

    Added fixed block size allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -7,10 +7,11 @@ use x86_64::{
     VirtAddr,
 };
 
-use self::{bump::BumpAllocator, linked_list::LinkedListAllocator};
+use self::{bump::BumpAllocator, linked_list::LinkedListAllocator, fixed_size_block::FixedSizeBlockAllocator};
 
 pub mod bump;
 pub mod linked_list;
+pub mod fixed_size_block;
 
 /// Align the given address `addr` upwards to alignment `align`.
 ///
@@ -46,9 +47,13 @@ fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
 // static ALLOCATOR: Locked<BumpAllocator> =
 //     Locked::new(BumpAllocator::new());
 
+// #[global_allocator]
+// static ALLOCATOR: Locked<LinkedListAllocator> =
+//     Locked::new(LinkedListAllocator::new());
+
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> =
-    Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(
+    FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
